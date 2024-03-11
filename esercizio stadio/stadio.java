fbdb61f418e3b94d06def71bb189031fc7ecb83d
Giacomo Arcangeli <129013704+Giacomo-Arcangeli@users.noreply.github.com>
2024-03-11 17:32:34 +0100

    incasso e spettatori weekend

--- esercizio stadio/stadio.java
+++ esercizio stadio/stadio.java
@@ -1,24 +1,49 @@
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class stadio {
 
     public static void main(String[] args) {
         Random random = new Random();
 
-        // preparo ID e Incasso
-        int idSpettatore = 0;
-        int incassoTotale = 0;
+        // numero di weekend da valutare
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Scegli il numero di weekend da valutare");
+        int risposta = myObj.nextInt();
 
-        // simulo incasso sabato
-        int spettatoriSabato = random.nextInt(70000) + 1;
-        int incassoSabato = spettatoriSabato * 20;
-        incassoTotale += incassoSabato;
-        idSpettatore += spettatoriSabato;
+        int numeroWeekend = risposta;
 
-        // simulo incasso domenica
-        int spettatoriDomenica = random.nextInt(70000) + 1;
-        int incassoDomenica = spettatoriDomenica * 15;
-        incassoTotale += incassoDomenica;
-        idSpettatore += spettatoriDomenica;
+        // preparo l'arrayList che conterrà gli incassi di ogni weekend
+        ArrayList<Integer> incassiWeekend = new ArrayList<>();
+
+        // preparo l'arrayList che conterrà gli spettatori di ogni weekend
+        ArrayList<Integer> spettatoriWeekend = new ArrayList<>();
+
+        int weekendCorrente = 0;
+
+        // calcolo il numero degli spettatori e l'incasso di ogni weekend
+        do {
+            int spettatoriSabato = random.nextInt(70000) + 1;
+            int incassoSabato = spettatoriSabato * 20;
+
+            int spettatoriDomenica = random.nextInt(70000) + 1;
+            int incassoDomenica = spettatoriDomenica * 15;
+
+            // INCASSO TOTALE DEL WEEKEND
+            int incassoTotaleWeekend = incassoSabato + incassoDomenica;
+            incassiWeekend.add(incassoTotaleWeekend);
+            weekendCorrente++;
+
+            // SPETTATORI TOTALI WEEKEND
+            int spettatoriTotaliWeekend = spettatoriSabato + spettatoriDomenica;
+            spettatoriWeekend.add(spettatoriTotaliWeekend);
+
+        } while (weekendCorrente < numeroWeekend);
+
+        // stampo
+        System.out.println("Incassi per weekend: " + incassiWeekend);
+        System.out.println("Spettatori per weekend: " + spettatoriWeekend);
+
     }
 }
