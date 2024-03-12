c45031d81519dc3dcae65a2c63dbd5e907246270
Giacomo Arcangeli <129013704+Giacomo-Arcangeli@users.noreply.github.com>
2024-03-12 16:36:06 +0100

    Primo commit
    inizio

---
+++ Esercizio Piatto Speciale/Menu.java
@@ -0,0 +1,8 @@
+public class Menu {
+
+    private String[] pane = { "Integrale", "Bianco", "Cereali" };
+    private int[] prezzoPane = { 2, 1, 3 };
+    private String[] condimento = { "Prosciutto", "Salame", "Nutella" };
+    private int[] prezzoCondimento = { 4, 3, 2 };
+
+}
---
+++ Esercizio Piatto Speciale/PiattoSpeciale.java
@@ -0,0 +1,9 @@
+
+public class PiattoSpeciale {
+
+    private String pane;
+    private String condimento;
+    private int prezzoPane;
+    private int prezzoCondimento;
+
+}
