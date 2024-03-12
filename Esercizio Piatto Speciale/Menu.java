b0ea1df720516a21911c560a72dc15139cc6c2f9
Giacomo Arcangeli <129013704+Giacomo-Arcangeli@users.noreply.github.com>
2024-03-12 17:37:18 +0100

    Manca la parte del menu

--- Esercizio Piatto Speciale/Menu.java
+++ Esercizio Piatto Speciale/Menu.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Menu {
 
-    private String[] pane = { "Integrale", "Bianco", "Cereali" };
-    private int[] prezzoPane = { 2, 1, 3 };
-    private String[] condimento = { "Prosciutto", "Salame", "Nutella" };
-    private int[] prezzoCondimento = { 4, 3, 2 };
+    public static String[] pane = { "Integrale", "Bianco", "Cereali" };
+    public static int[] prezzoPane = { 2, 1, 3 };
+    public static String[] condimento = { "Prosciutto", "Salame", "Nutella" };
+    public static int[] prezzoCondimento = { 4, 3, 2 };
 
+    public static void main(String[] args) {
+
+        // devo chiedere all'utente di comporre il suo panino
+
+    }
 }
