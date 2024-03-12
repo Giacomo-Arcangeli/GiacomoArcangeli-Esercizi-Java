3794f7ee06b1026c9618aa711c59f544f12f6ac1
Giacomo Arcangeli <129013704+Giacomo-Arcangeli@users.noreply.github.com>
2024-03-12 17:04:03 +0100

    Piatto Speciale
    Ho fatto la classe PiattoSpeciale

--- Esercizio Piatto Speciale/PiattoSpeciale.java
+++ Esercizio Piatto Speciale/PiattoSpeciale.java
@@ -1,9 +1,41 @@
 
 public class PiattoSpeciale {
 
+    // ATTRIBUTI
     private String pane;
     private String condimento;
     private int prezzoPane;
     private int prezzoCondimento;
+    private int prezzoTotalePiatto;
+
+    // COSTRUTTORE
+    public PiattoSpeciale(String pane, int prezzoPane, String condimento, int prezzoCondimento) {
+        this.pane = pane;
+        this.prezzoPane = prezzoPane;
+        this.condimento = condimento;
+        this.prezzoCondimento = prezzoCondimento;
+        this.prezzoTotalePiatto = prezzoPane + prezzoCondimento + 1;
+    }
+
+    // METODI GET
+    public String getPane() {
+        return pane;
+    }
+
+    public String getCondimento() {
+        return condimento;
+    }
+
+    public int getPrezzoPane() {
+        return prezzoPane;
+    }
+
+    public int getPrezzoCondimento() {
+        return prezzoCondimento;
+    }
+
+    public int getPrezzoTotalePiatto() {
+        return prezzoTotalePiatto;
+    }
 
 }
