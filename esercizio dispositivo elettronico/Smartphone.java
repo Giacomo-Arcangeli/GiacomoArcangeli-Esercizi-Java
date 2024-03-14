2eb139097e438f93c116d5c17eca9333112495b9
Giacomo Arcangeli <129013704+Giacomo-Arcangeli@users.noreply.github.com>
2024-03-14 11:38:10 +0100

    smartphone

--- esercizio dispositivo elettronico/Smartphone.java
+++ esercizio dispositivo elettronico/Smartphone.java
@@ -1,16 +1,38 @@
+import java.util.Scanner;
 
 class Smartphone extends DispositivoElettronico {
 
+    public String usernameUtente;
+    public String passwordUtente;
+
+    // scrivo il metodo login
+    private void login(String username, String password) {
+        this.usernameUtente = username;
+        this.passwordUtente = password;
+        System.out.println("Username: " + username + ", Password: " + password);
+    }
+
+    // aggiungo il metodo login dentro ad avviaApplicazione
     @Override
     public void avviaApplicazione(String nomeApp) {
         super.avviaApplicazione(nomeApp);
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci l'username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Inserisci la password: ");
+        String password = scanner.nextLine();
         // login
-        login("Giacomo", "12345678");
+        login(username, password);
 
     }
 
-    private void login(String username, String password) {
-        System.out.println("Username: " + username + ", Password: " + password);
-    }
+    // prova
+
+    // public static void main(String[] args) {
+    // Smartphone smartphone = new Smartphone();
+    // smartphone.avviaApplicazione("Facebook");
+    // }
 }
