bd179c3349bf7a2605db14f6937394a933a32746
Giacomo Arcangeli <129013704+Giacomo-Arcangeli@users.noreply.github.com>
2024-03-14 12:22:40 +0100

    Tablet

---
+++ esercizio dispositivo elettronico/ResocontoDigitale.java
@@ -0,0 +1,7 @@
+public class ResocontoDigitale {
+
+    // creo un metodo che stampa tutti i dati
+    public static void stampaResoconto(Smartphone smartphone, Tablet tablet) {
+        System.out.println();
+    }
+}
